13d56204de32f970e9f579e5e5732d827610d187
Tom Harel <tomharel93@gmail.com>
2019-06-13 21:11:25 +0300

    face detection, face recognition and images to the DB

---
+++ FaceDetaction.py
@@ -0,0 +1,32 @@
+import cv2
+
+
+frontalFaceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+profileFaceCascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = frontalFaceCascade.detectMultiScale(gray, 1.3, 5)
+    profileFaces = profileFaceCascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+    for (x, y, w, h) in profileFaces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ faceRecognition.py
@@ -0,0 +1,50 @@
+import face_recognition
+from PIL import Image, ImageDraw, ImageFont
+from os import walk
+
+def get_suspects_info(path):
+    suspects_encodings = []
+    suspects_names = []
+    for x in walk(path):
+        for i in x[2]:
+            suspect_img = face_recognition.load_image_file(path + "/" + i)
+            suspect_img_encoding = face_recognition.face_encodings(suspect_img)[0]
+            suspects_encodings.append(suspect_img_encoding)
+            suspects_names.append(i.split(".")[0])
+    return suspects_encodings, suspects_names
+
+def find_match(unknown):
+    unknown_face_coordinates = face_recognition.face_locations(unknown)
+    unknown_encoding = face_recognition.face_encodings(unknown, unknown_face_coordinates)
+
+    suspects_encodings, suspects_names = get_suspects_info('./images/suspects')
+
+    pil_img = Image.fromarray(unknown)
+    draw = ImageDraw.Draw(pil_img)
+    for (top, right, bottom, left), face_encoding in zip(unknown_face_coordinates, unknown_encoding):
+        matches = face_recognition.compare_faces(suspects_encodings, face_encoding, tolerance=0.52)
+
+        name = "???"
+
+        if True in matches:
+            match_index = matches.index(True)
+            name = suspects_names[match_index]
+
+        draw.rectangle(((top, left), (right, bottom)), outline=(0, 0, 0))
+
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 0)
+                       , outline=(0, 0, 0))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255),
+                  font=ImageFont.truetype("arial.ttf", 18))
+    del draw
+
+
+    pil_img.show()
+
+image = face_recognition.load_image_file('./images/buffer/Yoav&Tom.jpg')
+image_encoding = face_recognition.face_encodings(image)[0]
+
+find_match(image)
+
+
